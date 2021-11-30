a03ed32aeda492caf3aa93655ed3039457a7ee78
hanseulhee <3021062@gmail.com>
2021-12-01 01:47:28 +0900

    feat: todo, quote design

--- js/greetings.js
+++ js/greetings.js
@@ -4,7 +4,7 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-  
+
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -12,7 +12,6 @@ function onLoginSubmit(event) {
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
-
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
--- js/todo.js
+++ js/todo.js
@@ -21,7 +21,7 @@ function paintToDo(newTodo) {
 
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "🗑";
+  button.innerText = "✔";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
