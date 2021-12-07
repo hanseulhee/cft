28e670d1ad528155a8d5cd6e056e147a598e05e7
hanseulhee <3021062@gmail.com>
2021-12-08 01:23:41 +0900

    docs: Update Readme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Cheer For Today
 
 ## Result
 
+![ezgif com-gif-maker (16)](https://user-images.githubusercontent.com/63100352/145066937-89468038-4385-4f62-b055-4845f29ab4ba.gif)
 
 
 - [Javascript 공부 기록](./docs/README.md)
--- js/greetings.js
+++ js/greetings.js
@@ -5,23 +5,22 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event) {
+function onSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
+  localStorage.setItem(USERNAME_KEY, username); // username저장
   paintGreetings(username);
 }
+
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.addEventListener("submit", onSubmit);
 } else {
   paintGreetings(savedUsername);
 }
